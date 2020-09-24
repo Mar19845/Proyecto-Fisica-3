ec948cd1fb2b0706c2733b64ae8c5a27f2efc1b0
Carlos21122000 <53351491+Carlos21122000@users.noreply.github.com>
2020-09-24 16:30:13 -0600

    Update Main.py

--- Main.py
+++ Main.py
@@ -3,27 +3,27 @@
 
 
 # Cargas a utilizar
-C_neutra = 0
-C_positiva = 1.6e-19
+C_neutra = 0 
+C_positiva = 1.6e-19 #medida en C
 C_negativa = -1.6e-19
 C_alfa = 3.2e-19
 
-# Masas de las particulas a utilizar
-M_electron = 9.109e-31
-M_positron = 9.109e-31
-M_proton = 1.675e-27
-M_neutron = 1.675e-27
-M_particulaAlfa = 6.696e-27
-M_nucleoDeDeuterio = 3.348e-27
-M_muon = 1.880e-25
-M_antimuon = 1.880e-25
-M_positron = 0 #falta
-M_positron = 0 #falta
+dicc_particulas = {"Electron": 9.109e-31,#masas, medida en Kilogramo
+                   "Positron" : 9.109e-31,
+                   "Proton" : 1.675e-27,
+                   "Neutron": 1.675e-27,
+                   "ParticulaAlfa": 6.696e-27,
+                   "NucleoDeDeuterio": 3.348e-27,
+                   "Muon": 1.880e-25,
+                   "Antimuon": 1.880e-25,
+                   "sabes": 0, #falta
+                   "saber": 0, #falta
+                    }
 
 
 print("Hola")
 print('''Bienvenido
-Al programa que resuelve ecuaciones diferenciales ''')
+Al programa grafica una particula en un campo electrico ''')
 
 
 cat=''
@@ -38,63 +38,14 @@ Ingrese la opcion que desea usar:  ''')
         cat=input('''
 1. Ingresar Valores para graficar 
 2. Fin de programa
-Ingrese la opcion que desea usar:  \n''')
+Ingrese la opcion que desea usar:  ''')
     if cat=='2':
         print("")
         print("Gracias por hacer uso del programa")
         break
     if cat=='1':
-        #ingreso de los valores de la velociadad inicial
-        print("Ingrese los siguientes valores \n\n")
-        while True:
-            try:
-                VelDir = float(input("Ingrese la VELOCIDAD INICIAL DE LA PARTICULA: "))
-                break
-            except:
-                print("Ingrese Un valor Valido\n")
-        while True:
-            try:
-                VelMagnitud = float(input("Ingrese EL ANGULO DE DISPARO DE 0 A 90 GRADOS: "))
-                break
-            except:
-                print("Ingrese Un valor Valido\n")
-        
-        #ingreso de los valores del campo electrico
-        
-       #print("Ingrese los valores del CAMPO ELECTRICO")
-        #magnitud del campo
-        while True:
-            try:
-                CampoMagnitud = float(input("Ingrese la MAGNITUD del CAMPO ELECTRICO: "))
-                break
-            except:
-                print("Ingrese Un valor Valido\n")
-        #sentido del campo
-        while True:
-            try:
-                print("Ingrese una de las opciones (1 o 2) para el SENTIDO del CAMPO ELECTRICO: ")
-                print("1. Eje y positivo")
-                print("2. Eje y negativo")
-                CampoSentido = int(input("Ingrese la opcion"))
-                break
-            except:
-                print("Ingrese Un valor Valido\n")
-#         #largo del campo, lo comente porque creo que no hace falta el largo
-#         while True:
-#             try:
-#                 CampoLargo = float(input("Ingrese el LARGO del CAMPO ELECTRICO: "))
-#                 break
-#             except:
-#                 print("Ingrese Un valor Valido")
-        #ancho del campo
-        while True:
-            try:
-                CampoAncho = float(input("Ingrese el ANCHO del CAMPO ELECTRICO:"))
-                break
-            except:
-                print("Ingrese Un valor Valido\n")
+        #tipo de particula
         print("\n")
-       #tipo de particula
         print("Escoga la particula a utilizar \n")
         print("1. Electron")
         print("2. Positron")
@@ -110,16 +61,94 @@ Ingrese la opcion que desea usar:  \n''')
         while True:
             try:
                 ParticulaTipo = int(input("Ingrese opcion"))
+                if ParticulaTipo == 1:
+                    #obtener el valor de carga
+                    masa = dicc_particulas["Electron"]
+                elif ParticulaTipo == 2:
+                    #obtener el valor de carga
+                    masa = dicc_particulas["Positron"]
+                elif ParticulaTipo == 3:
+                    #obtener el valor de carga
+                    masa = dicc_particulas["Proton"]
+                elif ParticulaTipo == 4:
+                    #obtener el valor de carga
+                    masa = dicc_particulas["Neutron"]
+                elif ParticulaTipo == 5:
+                    #obtener el valor de carga
+                    masa = dicc_particulas["ParticulaAlfa"]
+                elif ParticulaTipo == 6:
+                    #obtener el valor de carga
+                    masa = dicc_particulas["NucleoDeDeuterio"]
+                elif ParticulaTipo == 7:
+                    #obtener el valor de carga
+                    masa = dicc_particulas["Muon"]
+                elif ParticulaTipo == 8:
+                    #obtener el valor de carga
+                    masa = dicc_particulas["Antimuon"]
+                elif ParticulaTipo == 9:
+                    #obtener el valor de carga
+                    masa = dicc_particulas["saber"]
+                elif ParticulaTipo == 10:
+                    #obtener el valor de carga
+                    masa = dicc_particulas["saber"]
+                    
                 break
             except:
                 print("Ingrese Una opcion Valida\n")
+        print(masa)
         while True:
             try:
-                ParticulaCantidad = int(input("Ingrese el NUMERO DE PARTIULAS que desea usar: "))
+                ParticulaCantidad = int(input("Ingrese el NUMERO DE PARTIULAS que desea usar:"))
+                break
+            except:
+                print("Ingrese Un valor Valido\n")
+        #ingreso de los valores de la velociadad inicial
+        while True:
+            try:
+                velocidad = float(input("Ingrese la VELOCIDAD INICIAL DE LA PARTICULA: "))
+                break
+            except:
+                print("Ingrese Un valor Valido\n")
+        while True:
+            try:
+                angulo = float(input("Ingrese EL ANGULO DE DISPARO DE 0 A 90 GRADOS: "))
                 break
             except:
                 print("Ingrese Un valor Valido\n")
         
+        #ingreso de los valores del campo electrico
+        
+       #print("Ingrese los valores del CAMPO ELECTRICO")
+        #magnitud del campo
+        while True:
+            try:
+                CMagnitud = float(input("Ingrese la MAGNITUD del CAMPO ELECTRICO: "))
+                break
+            except:
+                print("Ingrese Un valor Valido\n")
+        #sentido del campo
+        while True:
+            try:
+                print("Ingrese una de las opciones (1 o 2) para el SENTIDO del CAMPO ELECTRICO: ")
+                print("1. Eje y positivo")
+                print("2. Eje y negativo")
+                CSentido = int(input("Ingrese la opcion"))
+                break
+            except:
+                print("Ingrese Un valor Valido\n")
+#         #largo del campo, lo comente porque creo que no hace falta el largo
+#         while True:
+#             try:
+#                 CampoLargo = float(input("Ingrese el LARGO del CAMPO ELECTRICO: "))
+#                 break
+#             except:
+#                 print("Ingrese Un valor Valido")
+        #ancho del campo
+        while True:
+            try:
+                CAncho = float(input("Ingrese el ANCHO del CAMPO ELECTRICO:"))
+                break
+            except:
+                print("Ingrese Un valor Valido\n")
         #333333333333333
-        print(M_electron)
-    
+        print(CAncho)
